beeec84329a178d02e9fe997d248cebe64a9d9e8
nikitabaladha <nikitabaladha1996@gmail.com>
2024-08-09 10:57:16 +0530

    Fixed button to scroll to top functionality added

--- skilledhyre/src/components/FixedButton/FixedButton.js
+++ skilledhyre/src/components/FixedButton/FixedButton.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaArrowUpLong } from "react-icons/fa6";
 import "./FixedButton.css";
 
 const FixedButton = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 500) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div className="fixed-icon">
-      <FaArrowUpLong />
+    <div>
+      <div
+        className={`fixed-icon ${showButton ? "show" : "hide"}`}
+        onClick={handleClick}
+      >
+        <FaArrowUpLong />
+      </div>
     </div>
   );
 };
